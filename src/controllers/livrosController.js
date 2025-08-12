e380dd66ee902ffde05ded793d9ed91ed5081e20
Diego Brito <diego.brito@totvs.com.br>
2025-08-12 15:48:42 -0300

    feat: criacao de middleware de paginacao

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,32 +1,18 @@
 import {autores, livros} from "../models/index.js";
-import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js"
 
 class LivroController {
 
   static listarLivros = async (req, res, next) => {
     try {
 
-      let { limite = 5, pagina = 1, campoOrdenacao = "_id", ordem = -1 } = req.query;
-
-      limite = parseInt(limite);
-      pagina = parseInt(pagina);
-      ordem = parseInt(ordem);
-
-      if (limite > 0 && pagina > 0) {
-        const livrosResultado = await livros.find()
-        .sort({ [campoOrdenacao]: ordem })
-        .skip((pagina - 1) * limite)
-        .limit(limite)
-        .populate("autor")
-        .exec();
-
-        res.status(200).json(livrosResultado);
-      } else {
-        next(new RequisicaoIncorreta());
-      }
-      
+      const buscaLivros = livros.find()
+      req.resultado = buscaLivros;
+      next();
+    
     } catch (erro) {
+     
       next(erro);
+    
     }
   }
 
@@ -85,11 +71,14 @@ class LivroController {
       const busca = await processaBusca(req.query);
 
       if (busca !== null) {
-        const livrosResultado = await livros
+
+        const livrosResultado = livros
         .find(busca)
         .populate("autor");
 
-        res.status(200).send(livrosResultado);
+        req.resultado = livrosResultado;
+
+        next();
       } else {
         res.status(200).send([]);
       }
